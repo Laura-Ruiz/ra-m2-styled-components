f6335e30a6569dda5978b2f7c3fd6ff3841f690e
Kevin Mamaqi <kevinmamaqi@gmail.com>
2022-12-18 15:23:07 +0100

    update

--- src/components/molecules/SearchBar.jsx
+++ src/components/molecules/SearchBar.jsx
@@ -4,7 +4,9 @@ import { Button, Icon, InputText } from '../atoms'
 export default function SearchBar() {
   return (
     <>
+      {/* Debería ser un select */}
       <InputText placeholder="Piso, chalet o garaje..." />
+      {/* Debería ser un select */}
       <InputText placeholder="Madrid, Barcelona o Zaragoza..." />
       <Button>
         <Icon size={27} name="search" />
